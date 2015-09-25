1ec273eaf747ecbcc0157fafb7aa75dc09cf7ead
crisrael <cmlucero37@gmail.com>
2015-09-25 15:53:05 -0700

    Create HelloWorld.adb

---
+++ HelloWorld.adb
@@ -0,0 +1,18 @@
+----------------------------------------
+-----  Hello World Initial Commit  -----
+----------------------------------------
+-- Date     -- Edited By          -- Comments
+-- 09/25/15    Crisrael Lucero    Created hello world in greatest language, Ada
+
+#if Team'Agrees then
+with Ada.Text_IO; use Ada.Text_IO;
+
+---------------------------------------
+-----    Procedure Hello World    -----
+---------------------------------------
+    procedure Hello is
+    begin
+       Put_Line ("Hello WORLD!");
+    end Hello; 
+    --END PROCEDURE
+#end if;
