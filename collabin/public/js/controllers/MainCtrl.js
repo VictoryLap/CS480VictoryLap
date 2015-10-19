1e0c7b5616b1878ec88d94ca3809d943828f23e9
Matt Dohlen <matt.dohlen@gmail.com>
2015-10-18 21:33:01 -0700

    Tested basic Angular functionalities

--- collabin/public/js/controllers/MainCtrl.js
+++ collabin/public/js/controllers/MainCtrl.js
@@ -2,4 +2,5 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
     $scope.tagline = 'An inventory for everyone';
+    
 });
--- collabin/public/js/controllers/NerdCtrl.js
+++ collabin/public/js/controllers/NerdCtrl.js
@@ -1,6 +1,31 @@
 // public/js/controllers/NerdCtrl.js
 angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
 
-    $scope.tagline = 'Nothing beats a pocket protector!';
+    $scope.inventory = [
+    	{'name': 'Full rack of boobies',
+    	 'amount': 2},
+    	{'name': 'Rack of Nuts',
+    	 'amount': 1},
+    	{'name': 'Harness',
+    	 'amount': 2},
+    	{'name': '70m Rope',
+    	 'amount': 2},
+    	{'name': 'GriGri',
+    	 'amount': 0},
+    	{'name': 'Stoke',
+    	 'amount': 9000}
 
+    ];  
+
+    $scope.orderProp = 'name';
+    $scope.orderType = "Amount";
+    $scope.sortOrder = function() {
+        if($scope.orderProp == 'amount') {
+            $scope.orderProp = 'name';
+            $scope.orderType = "Amount";
+        } else {
+            $scope.orderProp = 'amount';
+            $scope.orderType = 'Alphabetic';
+        }
+    };
 });
