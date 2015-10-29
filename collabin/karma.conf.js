3329a7308025c3e47440ef9192406acdc3a9772b
Matt Dohlen <matt.dohlen@gmail.com>
2015-10-29 09:58:49 -0700

    Added code coverage dependencies and ran one example

--- collabin/karma.conf.js
+++ collabin/karma.conf.js
@@ -32,13 +32,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+	'public/js/controllers/*.js': ['coverage']
+    },
+
+    // configure reporter
+    coverageReporter: {
+        type : 'html',
+	dir : 'test/coverage/'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
